635125ccf5f7245de3749fe47720e94f0ffcd70e
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2020-09-29 16:57:45 +0300

    fix: minor fix

--- src/02-practice/tasks.ts
+++ src/02-practice/tasks.ts
@@ -4,7 +4,7 @@ import { fromFetch } from "rxjs/fetch";
 import { ajax } from "rxjs/ajax";
 import { addItem, run } from './../03-utils';
 
-// Task 1. of
+// Task 1. of()
 // Реализуйте тело функции, которая принимает переменное количество параметров 
 // и создает Observable, который выдает значения ее аргументов
 (function task1(...rest: any[]): void {
@@ -13,7 +13,7 @@ import { addItem, run } from './../03-utils';
     // run(stream$);
 })(1, 'string', true, {});
 
-// Task 2.1 from
+// Task 2.1 from()
 // Реализуйте тело функции, которая принимает на вход массив и создает Observable,
 // который выдает значения этого массива
 (function task2(arr: any[]): void {
@@ -23,7 +23,7 @@ import { addItem, run } from './../03-utils';
 })([1, 'string', true, {}]);
 
 
-// Task 2.2. from
+// Task 2.2. from()
 // Реализуйте тело функции, которая создает Observable, который выдает случайные числа в дианазоне от min до max
 // используя генератор. Верните 10 чисел, используя take()
 (function task3() {
@@ -38,7 +38,7 @@ import { addItem, run } from './../03-utils';
     // run(stream$);
 })();
 
-// Task 3 fromEvent
+// Task 3 fromEvent()
 // Реализуйте тело функции, которая принимает 
 // id кнопки и создает Observable, который выдает значения времени клика по кнопке
 (function task3(buttonId: string): void {
@@ -47,7 +47,7 @@ import { addItem, run } from './../03-utils';
     // run(stream$);
 })('runBtn');
 
-// Task 4. fromEventPattern
+// Task 4. fromEventPattern()
 // Реализуйте функцию, которая создаст Observable, который выдает значения,
 // передаваемые вызову методу emit();
 (function task4() {
@@ -78,7 +78,7 @@ import { addItem, run } from './../03-utils';
 
 // Task 5. fromFetch()
 // Реализуйте функцию, которая создает Observable, который выдает имена пользователей. 
-// Используйте операторы: fromFetch('http://jsonplaceholder.typicode.com/users), filter(), switchMap(), map()
+// Используйте операторы: fromFetch('http://jsonplaceholder.typicode.com/users'), filter(), switchMap(), map()
 (function task5() {
     // const stream$ = 
 
@@ -94,7 +94,7 @@ import { addItem, run } from './../03-utils';
     // run(stream$);
 })();
 
-// Task7. interval
+// Task7. interval()
 // Реализуйте функцию, которая создает Observable, который запрашивает и выдает имена ползователей каждые 5с 
 // Используйте операторы: ajax('http://jsonplaceholder.typicode.com/users'), switchMap(), map()
 (function task7() {
@@ -134,7 +134,7 @@ import { addItem, run } from './../03-utils';
 // Task 10. pairs()
 // Реализуйте функцию, которая создает Observable.
 // Пусть есть поток objAddressStream, который выдает объект и второй поток fieldsStream, который содержит перечень ключей объекта
-// Необходимо модифицировать поток так, чтобы он выдавал объект только с дынными ключей из 
+// Необходимо модифицировать поток так, чтобы он выдавал объект только с данными ключей из 
 // второго потока. 
 // Используйте pairs(), switchMap(), reduce(), filter(), withLatestFrom()
 (function task10() {
@@ -179,7 +179,7 @@ import { addItem, run } from './../03-utils';
     // run(stream$);
 })();
 
-// Task 12. throwError
+// Task 12. throwError()
 // Реализуйте функцию, которая создаст Observable, который завершиться с ошибкой, если в массиве встретится число 3.
 // Используейте from, switchMap, of, throwError
 (function task11() {
@@ -190,7 +190,7 @@ import { addItem, run } from './../03-utils';
     // run(stream$);
 })();
 
-// Task 14. bindCallback
+// Task 14. bindCallback()
 // Пусть есть некоторая функция doAsyncJob, которая выполняет асинхронную операцию и вызывает колбек, 
 // когда эта операция завершается.
 // Используя bindCallback, создайте функцию reactiveDoAsyncJob, вызовов которой создаст поток с передаваемым ей значением.
@@ -209,7 +209,7 @@ import { addItem, run } from './../03-utils';
     // run(stream$);
 })();
 
-// Task 15. bindNodeCallback
+// Task 15. bindNodeCallback()
 // Пусть есть некоторая функция doAsyncJob, которая выполняет асинхронную операцию и вызывает колбек в "формате ноды", 
 // когда эта операция завершается.
 // Используя bindNodeCallback, создайте функцию reactiveDoAsyncJob, вызовов которой создаст поток,
@@ -228,7 +228,7 @@ import { addItem, run } from './../03-utils';
     // run(stream$);
 })();
 
-// Task 16. defer
+// Task 16. defer()
 // Пусть есть некоторая функция getUsers(), которая возвращает коллекцию пользователей с помощью fetch()
 // Создать Observable, в котром запуск функции getUser() произойдет в момент подписки на поток
 // Используйте defer, switchMap
@@ -249,7 +249,7 @@ import { addItem, run } from './../03-utils';
 
 
 
-// Task 17. generate
+// Task 17. generate()
 // Реализуйте функцию, которая создает Observable, который будет выдавать в поток значения, 
 // хранящихся в свойстве sequence класса С
 (function task17() {
